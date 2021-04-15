0de9aae4cb2771b05a6569d963a07906b2b15c8a
Matt Mitchell <matt.mitchell@nrel.gov>
2021-04-14 20:19:20 -0600

    add material and construction objects, create construction checksum

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 unsigned long long roundWithPrecision_to_ullong(double &x, double const &precision) {
 	// rounds to unsigned long long after multiplying by precision
-	return (unsigned long long) (x * precision + .5);;
+	return (unsigned long long) (x * precision + .5);
 }
 
 std::string bitsetAddFloats(std::vector<double> &vals, double const &precision) {
@@ -58,6 +58,65 @@ std::string bitsetAddFloats(std::vector<double> &vals, double const &precision) 
 	return result.to_string();
 }
 
+struct Material {
+
+	// must match the number of values in this struct to include in the checksum
+	constexpr static int numPropsToUseInChecksum = 3;
+
+	double conductivity;
+	double density;
+	double specificHeat;
+
+	Material() : conductivity(0.0), density(0.0), specificHeat(0.0) {}
+
+	Material(double conductivity, double density, double specificHeat) {
+		this->conductivity = conductivity;
+		this->density = density;
+		this->specificHeat = specificHeat;
+	}
+};
+
+struct Construction {
+
+	// must match the number of values in this struct to include in the checksum
+	constexpr static int numPropsToUseInChecksum = 1;
+
+	double resistance;
+	std::vector<Material> materials;
+
+	Construction() : resistance(0.0) {}
+
+	explicit Construction(std::vector<Material> materials) { this->materials = materials; }
+	explicit Construction(double resistance) { this->resistance = resistance; }
+};
+
+std::string constructionChecksum(Construction const &construction) {
+
+	// compute checksum for construction
+
+	// offset by prime numbers, hopefully there's less of a chance of reversed numbers adding to the same value
+	constexpr double conductivityChecksumOffset = 7;
+	constexpr double densityChecksumOffset = 13;
+	constexpr double specificHeatChecksumOffset = 29;
+	constexpr double resistanceChecksumOffset = 59;
+
+	auto &materials = construction.materials;
+
+	// collect material values for checksum
+	std::vector<double> checksumInputs;
+	for (auto &m : materials) {
+		checksumInputs.push_back(m.conductivity * conductivityChecksumOffset);
+		checksumInputs.push_back(m.density * densityChecksumOffset);
+		checksumInputs.push_back(m.specificHeat * specificHeatChecksumOffset);
+	}
+
+	// collect
+	checksumInputs.push_back(construction.resistance * resistanceChecksumOffset);
+
+	constexpr double precision = 1E9;
+	return bitsetAddFloats(checksumInputs, precision);
+}
+
 int main() {
 
 	// values for testing standalone functions
@@ -66,9 +125,24 @@ int main() {
 
 	// result from using function
 	std::vector<double> toChecksum = {testVal1, testVal2};
-	const double precision = 1E1;
+	const double precision = 1E0;
 	std::string cs = bitsetAddFloats(toChecksum, precision);
 	std::cout << cs << std::endl;
+
+	// test materials
+	Material m1 = Material(10.0, 1000.0, 3990.0);// sum: 5000
+	Material m2 = Material(20.0, 990.0, 3990.0); // sum: 5000
+
+	std::vector<Material> materials = {m1, m2};
+	std::vector<Material> materialsReversed = {m2, m1};
+
+	// test constructions
+	Construction c1 = Construction(materials);
+	Construction c2 = Construction(materialsReversed);
+	Construction c3 = Construction(5000);// resistance = 5000
+	std::cout << constructionChecksum(c1) << std::endl;
+	std::cout << constructionChecksum(c2) << std::endl;
+	std::cout << constructionChecksum(c3) << std::endl;
 
 	return 0;
 }
